d0d0afa4b73f2454e4629aeab527866632b38f27
huyvouit <huyvouit@gmail.com>
2021-07-06 13:47:45 +0700

    add date()

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ const counter = document.querySelector('.counter');
 const btn = document.querySelector('.buttons');
 const secondsInput = document.getElementById('seconds');
 const btnReStart = document.getElementById('restart');
-
+const time_date = document.querySelector('.time-date');
 var seconds;
 var minuts;
 var remseconds;
 var toCount;
 
+var d = new Date();
+time_date.innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+setInterval(myFunction, 1000);
+function myFunction() {
+    d = new Date();
+    time_date.innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+}
 function subm(){
     display("submit", "start");
     seconds = Number(secondsInput.value);
